bd97f151498bf50899beb96a085e449db408f79e
Manuel Quinones <mquin2003@gmail.com>
2023-06-12 23:49:45 -0400

    Updated Again (2x)

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import "./About.css";
+import profile_pic from "../assets/20230612_231936.jpg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page. </p> 
+        <div>
+          <div className="split left">
+            <div className="centered">
+              <img 
+                className="profile_image"
+                src={profile_pic}
+                alt="Profile Pic"
+              ></img>
+            </div>
+          </div>
+          <div className="split right">
+            <div className="centered">
+              <div className="name_title">Manuel Quinones</div>
+              <div className="brief_description">
+                <h2>I am a student at Queens College. I enjoy experimenting with computers during my free time.</h2>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
